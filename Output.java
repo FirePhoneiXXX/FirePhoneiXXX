dd2346859cc058a4e7e368bf54b35ffa9a2ee8bc
FirePhoneiXXX <149240126+FirePhoneiXXX@users.noreply.github.com>
2023-12-18 17:30:40 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,8 @@
+package Rohit;
+
+public class Output {
+	public static void main (String [] args) {
+		System.out.println("*\n**\n***\n****");
+	}
+
+}
---
+++ input.java
@@ -0,0 +1,18 @@
+package Rohit;
+import java.util.Scanner;
+
+public class input {
+public static void main (String[]args){
+	Scanner rm = new Scanner(System.in);
+	System.out.println("print First numer");
+	float a= rm.nextFloat();
+	System.out.println("print Second numer");
+	float b=rm.nextFloat();
+	System.out.println("Sum of a+b is");
+	System.out.println(a+b/20);
+	
+	}
+	
+	
+	
+}
